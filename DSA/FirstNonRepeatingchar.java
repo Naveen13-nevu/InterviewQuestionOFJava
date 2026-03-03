9f0de741c1a8b8628e2c56354f7d5587393008dc
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-03-03 23:50:29 +0530

    DSA

---
+++ DSA/FirstNonRepeatingchar.java
@@ -0,0 +1,24 @@
+public class FirstNonRepeatingchar {
+    public static void main(String[] args) {
+        String str = "aabbcddee";
+
+        for(int i=0;i<str.length();i++){
+            boolean isRepeating = false;
+
+            for(int j=0;j<str.length();j++){
+                
+             if(i!= j && str.charAt(i)==str.charAt(j)){
+                isRepeating = true;
+                break;
+                }
+            }
+            if(!isRepeating){
+                System.out.println(str.charAt(i));
+            }
+        }
+
+
+
+
+    }
+}
