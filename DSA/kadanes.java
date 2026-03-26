165b0a261e0acb98f5c3953fe62e5873e2347cde
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-03-26 12:05:09 +0530

    DSA

--- DSA/kadanes.java
+++ DSA/kadanes.java
@@ -1,10 +1,10 @@
 class Kadanes{
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
-       int max = arr[0];
-       int sum = arr[0];
+        int max = arr[0];
+        int sum = arr[0];
 
-       for(int i=0;i<arr.length;i++){
+       for(int i=1;i<arr.length;i++){
         if(sum<0){
             sum = arr[i];
         }
