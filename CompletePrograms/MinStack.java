681ce20c2c9b5055b715e9ef8c3e02b31ab66020
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-04-04 21:23:59 +0530

    DSA

---
+++ CompletePrograms/MinStack.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class MinStack {
+    
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public void push(int n){
+        stack.push(n);
+        if(minStack.isEmpty() || n<=minStack.peek()){
+            minStack.push(n);
+        }
+    }
+
+    public void pop(){
+        int top = stack.peek();
+        while(!stack.isEmpty() && top == minStack.peek()){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top(){
+        return  stack.peek();
+    }
+
+    public int getMin(){
+        return minStack.peek();
+    }
+
+
+
+public static void main(String[] args) {
+    
+    MinStack min = new MinStack();
+    min.push(5);
+    min.push(3);
+    System.out.println(min.getMin());
+
+
+}
+}
