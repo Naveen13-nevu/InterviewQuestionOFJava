cc0b7e6d69cf892b0027057add3bff8b6185d5d4
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-04-06 22:34:48 +0530

    DSA

---
+++ CompletePrograms/SubarraySumEqualsK.java
@@ -0,0 +1,36 @@
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+
+    public static int subArray(int arr[], int k){
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int sum=0;
+        int count=0;
+        map.put(0,1);
+
+
+        for(int i=0;i<arr.length;i++){
+            sum = sum + arr[i];
+            if(map.containsKey(sum-k))
+            {
+                count = count + map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+
+        return count;
+    }
+    
+
+    public static void main(String[] args) {
+        
+        int arr[] = {1,1,1};
+        int k =2;
+        int result = subArray(arr,k);
+        System.out.println(result);
+
+    }
+}
