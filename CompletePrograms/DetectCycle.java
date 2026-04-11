e9b3ba2f34afd0579e5121adb2bd7977952af070
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-04-11 20:40:25 +0530

    DSA

---
+++ CompletePrograms/DetectCycle.java
@@ -0,0 +1,56 @@
+
+public class DetectCycle {
+
+    static  class ListNode{
+
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+        
+    }
+
+    public static boolean hasCycle(ListNode head){
+
+        if(head == null || head.next == null){
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return  true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+
+        head.next = second;
+        second.next = third;
+        third.next= fourth;
+        fourth.next = fifth;
+        fifth.next = third;
+
+        boolean result = hasCycle(head);
+        
+        System.out.println(result);
+
+    }
+}
