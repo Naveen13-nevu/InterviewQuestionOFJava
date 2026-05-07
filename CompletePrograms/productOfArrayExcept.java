b30365510b2185eda0a3534b07262b3a1f7b62aa
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-05-07 15:28:13 +0530

    DSA

--- CompletePrograms/productOfArrayExcept.java
+++ CompletePrograms/productOfArrayExcept.java
@@ -11,7 +11,7 @@ public class productOfArrayExcept {
         for(int i=1;i<n;i++){
             left[i] = left[i-1]* arr[i-1];
         }
-
+    
         right[n-1] = 1;
         for(int i= n-2;i>=0;i--){
             right[i] = right[i+1] * arr[i+1];
